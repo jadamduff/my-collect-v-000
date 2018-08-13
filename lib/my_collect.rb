b932fe8949c1b2206a6010558ee62d382605c8af
Adam Duff <j.adam.duff@gmail.com>
2018-08-13 10:22:57 +0000

    automatically backed up by learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -2,7 +2,7 @@ def my_collect(array)
   i = 0
   collection = []
   while 1 < array.length
-    collection << yield array[i]
+    collection << yield(array[i])
     i += 1
   end
   return collection
